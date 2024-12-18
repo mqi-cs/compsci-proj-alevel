620b6a6666b128ba2878b6ec92a7370ed6fe4962
mqi-cs <mqasimimran@outlook.com>
2024-12-18 17:22:17 +0000

    made a table with column of word list

--- database.py
+++ database.py
@@ -1,4 +1,20 @@
-import sqlite3 
+import sqlite3  
 
-conn = sqlite3.connect('wordle.db')     # variable = lang.func('name of databse')  #this
+conn = sqlite3.connect('wordle.db')     # variable = lang.func('name of databse') #connects to db #db creation
+
+#db creation above
+
+#table creation below
+
+c = conn.cursor                            # cursor variable = connectoing and making cursor vairbale, # create a cursor
+
+c.execute(""" CREATE TABLE wordle_list  (               
+
+        word_list test                                   
+""")                               #cursor command   #multiple ' for lines readibility , #column name and datatype
+
+
+conn.commit()            # commit the command
+
+conn.close()     #not neccesary but best practice,  # close ur connection
 
