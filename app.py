122fcfc955b102f7b5bdda310d2465c9191d4b48
mqi-cs <mqasimimran@outlook.com>
2024-12-19 16:55:53 +0000

    imported pygame and intialised a pygame window

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 print('Hello World!')
+
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+# This line creates a window of 500 width, 500 height
+
+pygame.display.set_caption("WORDLE!!!")   #change window name
